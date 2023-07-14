a6f7cd523c59c9e6e02a0d0d0bb9abf1e1835462
addoxy <aditya132003@gmail.com>
2023-07-14 20:14:15 +0530

    Fix border bottom issue of features card

--- src/components/Cards/FeaturesCard/FeaturesCard.tsx
+++ src/components/Cards/FeaturesCard/FeaturesCard.tsx
@@ -3,7 +3,7 @@ import { Icon } from "@iconify/react";
 const FeaturesCard = () => {
   return (
     <div className="grid h-80 w-full grid-cols-none grid-rows-3 rounded-2xl border border-neutral-200 lg:grid-cols-3 lg:grid-rows-none">
-      <div className="h-full border-b border-neutral-200 lg:border-r">
+      <div className="h-full border-b border-neutral-200 lg:border-b-0 lg:border-r">
         <div className="flex">
           {/* <Icon icon="" /> */}
           <div className="flex flex-col">
@@ -19,7 +19,7 @@ const FeaturesCard = () => {
           </div>
         </div>
       </div>
-      <div className="h-full border-b border-neutral-200 lg:border-r">
+      <div className="h-full border-b border-neutral-200 lg:border-b-0 lg:border-r">
         <div className="flex">
           {/* <Icon icon="" /> */}
           <div className="flex flex-col">
