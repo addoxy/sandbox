d46900a15ca04e101d0c879c3b8b1bcaf2825cec
addoxy <aditya132003@gmail.com>
2023-07-14 19:11:34 +0530

    Add features card shell

--- src/components/Cards/FeaturesCard/FeaturesCard.tsx
+++ src/components/Cards/FeaturesCard/FeaturesCard.tsx
@@ -5,14 +5,14 @@ const FeaturesCard = () => {
     <div className="grid h-80 w-full grid-cols-none grid-rows-3 rounded-2xl border border-neutral-200 lg:grid-cols-3 lg:grid-rows-none">
       <div className="h-full border-b border-neutral-200 lg:border-r">
         <div className="flex">
-          <Icon icon="" />
+          {/* <Icon icon="" /> */}
           <div className="flex flex-col">
             <span></span>
             <p></p>
           </div>
         </div>
         <div className="flex">
-          <Icon icon="" />
+          {/* <Icon icon="" /> */}
           <div className="flex flex-col">
             <span></span>
             <p></p>
@@ -21,14 +21,14 @@ const FeaturesCard = () => {
       </div>
       <div className="h-full border-b border-neutral-200 lg:border-r">
         <div className="flex">
-          <Icon icon="" />
+          {/* <Icon icon="" /> */}
           <div className="flex flex-col">
             <span></span>
             <p></p>
           </div>
         </div>
         <div className="flex">
-          <Icon icon="" />
+          {/* <Icon icon="" /> */}
           <div className="flex flex-col">
             <span></span>
             <p></p>
@@ -37,14 +37,14 @@ const FeaturesCard = () => {
       </div>
       <div className="h-full border-neutral-200">
         <div className="flex">
-          <Icon icon="" />
+          {/* <Icon icon="" /> */}
           <div className="flex flex-col">
             <span></span>
             <p></p>
           </div>
         </div>
         <div className="flex">
-          <Icon icon="" />
+          {/* <Icon icon="" /> */}
           <div className="flex flex-col">
             <span></span>
             <p></p>
