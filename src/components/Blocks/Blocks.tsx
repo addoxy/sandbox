c5e5c98df51678cc13c6e83da715786595c36a4a
addoxy <aditya132003@gmail.com>
2023-07-15 22:01:56 +0530

    Add get started block

--- src/components/Blocks/Blocks.tsx
+++ src/components/Blocks/Blocks.tsx
@@ -17,7 +17,7 @@ export const StartedBlock = () => {
         <span className="mb-6 flex items-center justify-center text-2xl font-semibold text-white sm:mb-10 sm:text-4xl md:text-5xl">
           Excited? Get Started Today!
         </span>
-        <button className="rounded-xl bg-white px-12 py-3 font-medium text-neutral-700 shadow-xl hover:text-blue-600 sm:px-20 sm:py-4 sm:text-lg">
+        <button className="rounded-xl bg-white px-12 py-3 font-medium text-neutral-700 shadow-xl hover:bg-neutral-100 hover:text-blue-600 sm:px-20 sm:py-4 sm:text-lg">
           Get Started
         </button>
       </div>
