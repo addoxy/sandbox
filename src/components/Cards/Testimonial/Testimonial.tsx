c1d6546690bafcffe7d6176ce404a7611aee76c9
addoxy <aditya132003@gmail.com>
2023-07-15 21:37:17 +0530

    Add testimonials section

--- src/components/Cards/Testimonial/Testimonial.tsx
+++ src/components/Cards/Testimonial/Testimonial.tsx
@@ -4,7 +4,7 @@ import woman from "@/../public/images/jpegs/woman.jpeg";
 const Testimonial = (props: { content: string }) => {
   return (
     <>
-      <div className="flex w-100 flex-col rounded-3xl border px-8 py-8 shadow-sm md:w-auto">
+      <div className="flex w-80 flex-col rounded-3xl border px-8 py-8 shadow-sm md:w-auto">
         <p className="mb-6 text-neutral-600">{props.content}</p>
         <div className="flex">
           <Image
