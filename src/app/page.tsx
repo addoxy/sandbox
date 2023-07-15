bb6a7dbf6eddb3c02ca78afcddd5f48fbaabe846
addoxy <aditya132003@gmail.com>
2023-07-15 14:32:30 +0530

    Add features heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,11 @@ export default function Home() {
       <div className="h-30 w-full sm:h-50"></div>
       <Block />
       <div className="h-30 w-full sm:h-50"></div>
+      <span className="mb-18 block text-3xl font-semibold text-neutral-700 sm:text-4xl lg:text-5xl">
+        Features that make us <span className="text-gradient"> stand out</span>
+      </span>
       <FeaturesCard />
-      <div className="h-50 w-full"></div>
+      <div className="h-30 w-full sm:h-50"></div>
     </>
   );
 }
