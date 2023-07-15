e8027ac1d93684c224be201a83ae666afcb67f5b
addoxy <aditya132003@gmail.com>
2023-07-15 13:15:14 +0530

    Change color of features card icons

--- src/components/Cards/FeaturesCard/IconContainer.tsx
+++ src/components/Cards/FeaturesCard/IconContainer.tsx
@@ -7,7 +7,7 @@ const IconContainer = (props: { icon: string; className: string }) => {
     <>
       <div className={props.className}>
         <div className="flex h-14 w-14 items-center justify-center rounded-full bg-blue-100">
-          <Icon icon={props.icon} className="h-6 w-6 text-blue-600" />
+          <Icon icon={props.icon} className="h-6 w-6 text-blue-400" />
         </div>
       </div>
     </>
