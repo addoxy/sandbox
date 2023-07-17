853ed967f3f63aec52e789a5dc7924472d9e6280
addoxy <aditya132003@gmail.com>
2023-07-17 21:23:32 +0530

    Code cleanup

---
+++ src/app/browse/data.tsx
@@ -0,0 +1,41 @@
+import os from "@/../public/images/os.jpg";
+import webdev from "@/../public/images/webdev.jpg";
+import design from "@/../public/images/design.jpg";
+
+export const courses = [
+  {
+    src: os,
+    name: "Operating Systems",
+    price: "$100",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium harum dolorem error tempore.",
+  },
+  {
+    src: webdev,
+    name: "Web Development",
+    price: "$500",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium harum dolorem error tempore repellendus molestiae.",
+  },
+  {
+    src: design,
+    name: "UI / UX",
+    price: "$200",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, molestiae!",
+  },
+  {
+    src: os,
+    name: "Operating Systems",
+    price: "$100",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium harum dolorem error tempore.",
+  },
+  {
+    src: webdev,
+    name: "Web Development",
+    price: "$500",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium harum dolorem error tempore repellendus molestiae.",
+  },
+];
