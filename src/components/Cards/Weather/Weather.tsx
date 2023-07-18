18dbb9e5d84a0c60d51d1c41f115592d19da60d0
addoxy <aditya132003@gmail.com>
2023-07-18 21:02:37 +0530

    Add weather card

--- src/components/Cards/Weather/Weather.tsx
+++ src/components/Cards/Weather/Weather.tsx
@@ -28,11 +28,19 @@ const Weather = async (props: { location: string }) => {
         <div className="weather-gradient ml-20 flex h-44 w-96 items-center justify-between rounded-2xl px-10 py-4 shadow-xl">
           <div className="flex flex-col">
             <p className="mb-3 text-5xl text-neutral-700">
-              {currentWeather.current.temp_c}°C
+              {currentWeather.current.temp_c
+                ? currentWeather.current.temp_c
+                : ""}
+              °C
             </p>
             <p className="text-sm text-neutral-600">
-              {currentWeather.location.region},{" "}
-              {currentWeather.location.country}
+              {currentWeather.location.region
+                ? currentWeather.location.region
+                : ""}
+              ,{" "}
+              {currentWeather.location.country
+                ? currentWeather.location.country
+                : ""}
             </p>
           </div>
           <SunIcon className="h-24 w-24 text-yellow-300" />
