7d9b99ec0136b1de5b4f10d687daf83bf510d1cb
addoxy <aditya132003@gmail.com>
2023-07-18 20:58:31 +0530

    Add weather card

--- src/components/Cards/Weather/Weather.tsx
+++ src/components/Cards/Weather/Weather.tsx
@@ -1,7 +1,17 @@
 import SunIcon from "@/utils/icons/tsx/SunIcon";
 import LocationInput from "./LocationInput";
 
-async function getWeather(location: string) {
+interface WeatherObject {
+  current: {
+    temp_c: string;
+  };
+  location: {
+    region: string;
+    country: string;
+  };
+}
+
+async function getWeather(location: string): Promise<WeatherObject> {
   const currentWeather = await fetch(
     `http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_KEY}&q=${location}`
   );
@@ -18,11 +28,11 @@ const Weather = async (props: { location: string }) => {
         <div className="weather-gradient ml-20 flex h-44 w-96 items-center justify-between rounded-2xl px-10 py-4 shadow-xl">
           <div className="flex flex-col">
             <p className="mb-3 text-5xl text-neutral-700">
-              {currentWeather.current.temp_c as string}°C
+              {currentWeather.current.temp_c}°C
             </p>
             <p className="text-sm text-neutral-600">
-              {currentWeather.location.region as string},{" "}
-              {currentWeather.location.country as string}
+              {currentWeather.location.region},{" "}
+              {currentWeather.location.country}
             </p>
           </div>
           <SunIcon className="h-24 w-24 text-yellow-300" />
