176762fe38c04299f507d252e10760bb97df3cbd
addoxy <aditya132003@gmail.com>
2023-07-18 20:42:30 +0530

    Add weather card

--- src/components/Cards/Weather/Weather.tsx
+++ src/components/Cards/Weather/Weather.tsx
@@ -10,9 +10,15 @@ async function getWeather(location: string) {
 
 const Weather = async (props: { location: string }) => {
   const currentWeather = await getWeather(props.location.trim());
-  const temperature = currentWeather.current.temp_c || " ";
-  const region = currentWeather.location.region || " ";
-  const country = currentWeather.location.country || " ";
+  if (!currentWeather.current.temp_c) {
+    currentWeather.current.temp_c = "";
+  }
+  if (!currentWeather.location.region) {
+    currentWeather.location.region = "";
+  }
+  if (!currentWeather.location.country) {
+    currentWeather.location.country = "";
+  }
 
   return (
     <>
@@ -20,9 +26,11 @@ const Weather = async (props: { location: string }) => {
         <LocationInput />
         <div className="weather-gradient ml-20 flex h-44 w-96 items-center justify-between rounded-2xl px-10 py-4 shadow-xl">
           <div className="flex flex-col">
-            <p className="mb-3 text-5xl text-neutral-700">{temperature}</p>
+            <p className="mb-3 text-5xl text-neutral-700">
+              {currentWeather.current.temp_c}
+            </p>
             <p className="text-sm text-neutral-600">
-              {region},{country}
+              {currentWeather.location.region},{currentWeather.location.country}
             </p>
           </div>
           <Sun className="h-24 w-24 text-yellow-300" />
